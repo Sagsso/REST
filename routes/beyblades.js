c285c248984fdd6e8788c03f2c3417e4f496c51b
sagsso <santiagogalvez17@gmail.com>
2020-05-12 12:51:44 -0500

    CRUD beyblades

--- routes/beyblades.js
+++ routes/beyblades.js
@@ -1,19 +1,56 @@
-module.exports = function(app){
+module.exports = function(app, db){
 
-    app.get('/bayblades', (req, res) => {
-        res.send(`[GET] ${req.url}`);
+    app.get('/beyblades', (req, res) => {
+        res.status(200).json(db.beyblades);
+    });
+
+    app.get('/beyblades/:owner', (req, res) => {
+        const beast = req.params.beast;
+        const beyblade = db.beyblades.filter(el => {
+            return el.beast == beast;
+        })
+        res.status(200).json(beyblade);
     });
     
-    app.post('/bayblades', (req, res) => {
-        res.status(200).json({ bayblades: [] });
+    app.post('/beyblades', (req, res) => {
+        const beyblade = {
+            beast: req.body.beast,
+            owner: req.body.owner,
+            team: req.body.team
+        };
+        db.beyblades.push(beyblade);
+        res.status(201).json({ msg: "Created", result: beyblade });
     });
     
-    app.put('/bayblades', (req, res) => {
-        res.send(`[PUT] ${req.url}`);
+    app.put('/beyblades/:beast', (req, res) => {
+        const beast = req.params.beast;
+        const team = req.body.team;
+        const owner = req.body.owner;
+        db.beyblades.filter(el => {
+            if (el.beast == beast) {
+                el.team = team;
+                el.owner = owner;
+            }
+        });
+        const beyblade = {
+            beast: beast,
+            team: team,
+            owner: owner
+        }
+        res.status(201).json({ msg: "Updated", result: beyblade });
     });
     
-    app.delete('/bayblades', (req, res) => {
-        res.send(`[DELETE] ${req.url}`);
+    app.delete('/beyblades/:beast', (req, res) => {
+        const beast = req.params.beast;
+        let beyblade;
+        db.beyblades.filter(el => {
+            if (el.beast === beast) {
+                let i = db.beyblades.indexOf(el);
+                beyblade = el;
+                i !== -1 && db.beyblades.splice(i, 1);
+            };
+        });
+        res.status(201).json({ msg: "Deleted", result: beyblade });
     });
 
 }
